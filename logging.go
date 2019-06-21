841387772851aec7f1260128207c497f24fe9a44
Björn Gerdau <bjoerngerdau@gmx.de>
2019-06-22 00:34:15 +0200

    feat: implement OpenTracingLogrusLogger

--- logging.go
+++ logging.go
@@ -1,6 +1,9 @@
 package turtleware
 
-import "strings"
+import (
+	"github.com/sirupsen/logrus"
+	"strings"
+)
 
 type loggingOptions struct {
 	logHeaders      bool
@@ -45,4 +48,32 @@ func LogHeaderBlacklist(headerBlacklist ...string) LoggingOption {
 			c.headerBlacklist[strings.ToLower(header)] = struct{}{}
 		}
 	}
+}
+
+// OpenTracingLogrusLogger is an implementation of the openTracing Logger interface
+// that delegates to a given Logrus logger. If the logger is nil, logrus.StandardLogger()
+// is used.
+//
+type OpenTracingLogrusLogger struct {
+	logger *logrus.Logger
+}
+
+// Error logs an error message as an Logrus Errorf.
+func (l *OpenTracingLogrusLogger) Error(msg string) {
+	logger := l.logger
+	if logger == nil {
+		logger = logrus.StandardLogger()
+	}
+
+	logger.Errorf("Tracer error: %s", msg)
+}
+
+// Infof logs an info message as an Logrus Tracef.
+func (l *OpenTracingLogrusLogger) Infof(msg string, args ...interface{}) {
+	logger := l.logger
+	if logger == nil {
+		logger = logrus.StandardLogger()
+	}
+
+	logger.Tracef("Tracer: "+msg, args...)
 }
