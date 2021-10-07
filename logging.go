6381043fe2db4e701a12998c35ef926da0c579f1
Björn Gerdau <bjoerngerdau@gmx.de>
2021-10-07 21:05:43 +0200

    refactor: move LoggingOption

--- logging.go
+++ logging.go
@@ -2,54 +2,7 @@ package turtleware
 
 import (
 	"github.com/sirupsen/logrus"
-
-	"strings"
 )
-
-type loggingOptions struct {
-	logHeaders      bool
-	headerWhitelist map[string]struct{}
-	headerBlacklist map[string]struct{}
-}
-
-// LoggingOption represents an option for the logging parameters.
-type LoggingOption func(*loggingOptions)
-
-// LogHeaders sets whether or not headers hould be logged.
-// The default is false.
-func LogHeaders(logHeaders bool) LoggingOption {
-	return func(c *loggingOptions) {
-		c.logHeaders = logHeaders
-	}
-}
-
-// LogHeaderWhitelist sets a whitelist of headers to allow.
-// Automatically replaces the blacklist if used.
-// The default is not set, which means "allow all".
-func LogHeaderWhitelist(headerWhitelist ...string) LoggingOption {
-	return func(c *loggingOptions) {
-		c.headerWhitelist = make(map[string]struct{}, len(headerWhitelist))
-		c.headerBlacklist = nil
-
-		for _, header := range headerWhitelist {
-			c.headerWhitelist[strings.ToLower(header)] = struct{}{}
-		}
-	}
-}
-
-// LogHeaderBlacklist sets a blacklist of headers to disallow.
-// Automatically replaces the whitelist if used.
-// The default is not set, which means "allow all".
-func LogHeaderBlacklist(headerBlacklist ...string) LoggingOption {
-	return func(c *loggingOptions) {
-		c.headerWhitelist = nil
-		c.headerBlacklist = make(map[string]struct{}, len(headerBlacklist))
-
-		for _, header := range headerBlacklist {
-			c.headerBlacklist[strings.ToLower(header)] = struct{}{}
-		}
-	}
-}
 
 // OpenTracingLogrusLogger is an implementation of the openTracing Logger interface
 // that delegates to a given Logrus logger. If the logger is nil, logrus.StandardLogger()
--- middleware_logging.go
+++ middleware_logging.go
@@ -9,6 +9,51 @@ import (
 	"time"
 )
 
+type loggingOptions struct {
+	logHeaders      bool
+	headerWhitelist map[string]struct{}
+	headerBlacklist map[string]struct{}
+}
+
+// LoggingOption represents an option for the logging parameters.
+type LoggingOption func(*loggingOptions)
+
+// LogHeaders sets whether or not headers hould be logged.
+// The default is false.
+func LogHeaders(logHeaders bool) LoggingOption {
+	return func(c *loggingOptions) {
+		c.logHeaders = logHeaders
+	}
+}
+
+// LogHeaderWhitelist sets a whitelist of headers to allow.
+// Automatically replaces the blacklist if used.
+// The default is not set, which means "allow all".
+func LogHeaderWhitelist(headerWhitelist ...string) LoggingOption {
+	return func(c *loggingOptions) {
+		c.headerWhitelist = make(map[string]struct{}, len(headerWhitelist))
+		c.headerBlacklist = nil
+
+		for _, header := range headerWhitelist {
+			c.headerWhitelist[strings.ToLower(header)] = struct{}{}
+		}
+	}
+}
+
+// LogHeaderBlacklist sets a blacklist of headers to disallow.
+// Automatically replaces the whitelist if used.
+// The default is not set, which means "allow all".
+func LogHeaderBlacklist(headerBlacklist ...string) LoggingOption {
+	return func(c *loggingOptions) {
+		c.headerWhitelist = nil
+		c.headerBlacklist = make(map[string]struct{}, len(headerBlacklist))
+
+		for _, header := range headerBlacklist {
+			c.headerBlacklist[strings.ToLower(header)] = struct{}{}
+		}
+	}
+}
+
 // statusWriter is a wrapper for a http.ResponseWriter for capturing
 // the http status code and content length
 // Source: https://www.reddit.com/r/golang/comments/7p35s4/how_do_i_get_the_response_status_for_my_middleware/dse5y4g
