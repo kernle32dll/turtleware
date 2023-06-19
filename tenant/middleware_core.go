29246d51ae347bdc276820634e6324648b773f33
Björn Gerdau <bjoerngerdau@gmx.de>
2023-06-19 23:52:04 +0200

    refactor: extract some common tenant code

--- tenant/middleware_core.go
+++ tenant/middleware_core.go
@@ -17,23 +17,6 @@ import (
 
 var emptyListHash = hex.EncodeToString(sha256.New().Sum(nil))
 
-type ctxKey int
-
-const (
-	// ctxTenantUUID is the context key used to pass down the tenant UUID.
-	ctxTenantUUID ctxKey = iota
-)
-
-var (
-	// ErrContextMissingTenantUUID is an internal error indicating a missing
-	// tenant UUID in the request context, whereas one was expected.
-	ErrContextMissingTenantUUID = errors.New("missing tenant UUID in context")
-
-	// ErrTokenMissingTenantUUID indicates that a requested was
-	// missing the tenant uuid.
-	ErrTokenMissingTenantUUID = errors.New("token does not include tenant uuid")
-)
-
 // ListHashFunc is a function for returning a calculated hash for a given subset of entities
 // of a given tenant, via the given paging, for a list endpoint.
 // The function may return sql.ErrNoRows or os.ErrNotExist to indicate that there are not
@@ -222,38 +205,4 @@ func ResourceCacheMiddleware(
 			h.ServeHTTP(w, r)
 		})
 	}
-}
-
-// UUIDMiddleware is a http middleware for checking tenant authentication details, and
-// passing down the tenant UUID if existing, or bailing out otherwise.
-func UUIDMiddleware() func(http.Handler) http.Handler {
-	return func(h http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			claims, err := turtleware.AuthClaimsFromRequestContext(r.Context())
-			if err != nil {
-				turtleware.WriteError(r.Context(), w, r, http.StatusInternalServerError, err)
-				return
-			}
-
-			tenantUUID, ok := claims["tenant_uuid"].(string)
-			if !ok || tenantUUID == "" {
-				turtleware.WriteError(r.Context(), w, r, http.StatusBadRequest, ErrTokenMissingTenantUUID)
-				return
-			}
-
-			h.ServeHTTP(
-				w,
-				r.WithContext(context.WithValue(r.Context(), ctxTenantUUID, tenantUUID)),
-			)
-		})
-	}
-}
-
-func UUIDFromRequestContext(ctx context.Context) (string, error) {
-	tenantUUID, ok := ctx.Value(ctxTenantUUID).(string)
-	if !ok {
-		return "", ErrContextMissingTenantUUID
-	}
-
-	return tenantUUID, nil
 }
