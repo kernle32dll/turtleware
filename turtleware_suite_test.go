f5e7a06f5ab94a9d27576507d9348511f63dcb93
Björn Gerdau <bjoerngerdau@gmx.de>
2023-06-19 23:52:22 +0200

    refactor: remove unused test functions from turtleware_suite_test

--- turtleware_suite_test.go
+++ turtleware_suite_test.go
@@ -1,36 +1,9 @@
 package turtleware_test
 
 import (
-	"github.com/kernle32dll/turtleware"
-
 	"context"
 	"net/http"
-	"net/http/httptest"
 )
-
-// ExpectedError creates an error output, as returned by turtleware.
-func ExpectedError(status int, errors ...error) []byte {
-	errorList := make([]string, len(errors))
-	for i, err := range errors {
-		errorList[i] = err.Error()
-	}
-
-	errObj := struct {
-		Status int      `json:"status"`
-		Text   string   `json:"text"`
-		Errors []string `json:"errors"`
-	}{
-		Status: status,
-		Text:   http.StatusText(status),
-		Errors: errorList,
-	}
-
-	rec := httptest.NewRecorder()
-
-	turtleware.EmissioneWriter.Write(rec, &http.Request{}, status, errObj)
-
-	return rec.Body.Bytes()
-}
 
 // ErrorHandlerCapture is a helper struct to capture errors for a turtleware.ErrorHandlerFunc.
 type ErrorHandlerCapture struct {
